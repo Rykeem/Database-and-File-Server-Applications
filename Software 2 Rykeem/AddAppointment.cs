8246a2183f596579b21387b6f603486ea08c1eb6
Rykeem Alexander <rykeemalex@gmail.com>
2024-10-23 19:23:32 -0400

    Made it so appointments have to be between 9am - 5pm EST and the start time has to be before the end time

--- Software 2 Rykeem/AddAppointment.cs
+++ Software 2 Rykeem/AddAppointment.cs
@@ -22,6 +22,8 @@ namespace Software_2_Rykeem
             dateTimePicker2.Format = DateTimePickerFormat.Custom;
             dateTimePicker1.CustomFormat = "yyyy-MM-dd hh:mm:ss tt";
             dateTimePicker2.CustomFormat = "yyyy-MM-dd hh:mm:ss tt";
+            dateTimePicker1.MinDate = DateTime.Today;
+            dateTimePicker2.MinDate = DateTime.Today;
             Connection.UserIDComboBox(UserIDCB1);
             Connection.CustomerIDComboBox(CustomerIDCB1);
             dataXX = data;
@@ -29,7 +31,49 @@ namespace Software_2_Rykeem
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
+            //DateTime dateTime = dateTimePicker1.Value;
 
+            
+            //TimeZoneInfo est = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            //DateTime datetimeEST = TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo.Local, est); // converts user's input to est 
+
+            ////checks if date is between monday and friday
+            //if (datetimeEST.DayOfWeek >= DayOfWeek.Monday && datetimeEST.DayOfWeek >= DayOfWeek.Friday)
+            //{
+            //    if (datetimeEST.TimeOfDay >= new TimeSpan(9,0,0) && datetimeEST.TimeOfDay <= new TimeSpan(17,0,0))
+            //    {
+                    
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Time has to be between 9AM and 5PM EST");
+            //    }
+
+            //}
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            //DateTime dateTime = dateTimePicker2.Value;
+
+
+            //TimeZoneInfo est = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            //DateTime datetimeEST = TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo.Local, est); // converts user's input to est 
+
+            ////checks if date is between monday and friday
+            //if (datetimeEST.DayOfWeek >= DayOfWeek.Monday && datetimeEST.DayOfWeek >= DayOfWeek.Friday)
+            //{
+            //    if (datetimeEST.TimeOfDay >= new TimeSpan(9, 0, 0) && datetimeEST.TimeOfDay <= new TimeSpan(17, 0, 0))
+            //    {
+
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Time has to be between 9AM and 5PM EST");
+            //    }
+
+            //}
         }
 
         private void CancelB1_Click(object sender, EventArgs e)
@@ -45,10 +89,56 @@ namespace Software_2_Rykeem
 
         private void SaveB1_Click(object sender, EventArgs e)
         {
-            Connection.AppointmentAdd(CustomerIDCB1.Text, UserIDCB1.Text, nameTB1.Text, dateTimePicker1.Value, dateTimePicker2.Value);
-            Connection.AppointmentDatabase(dataXX);
-            this.Close();
-            Customer.Instance.Show();
+            DateTime dateTime = dateTimePicker1.Value; //start time 
+
+
+            TimeZoneInfo est = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime datetimeEST = TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo.Local, est);
+
+
+            DateTime dateTime2 = dateTimePicker2.Value; // end time 
+
+
+            TimeZoneInfo est2 = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime datetimeEST2 = TimeZoneInfo.ConvertTime(dateTime2, TimeZoneInfo.Local, est2);
+
+            if ( dateTime < dateTime2)
+            {
+                if (datetimeEST.DayOfWeek >= DayOfWeek.Monday && datetimeEST.DayOfWeek <= DayOfWeek.Friday && datetimeEST2.DayOfWeek >= DayOfWeek.Monday && datetimeEST2.DayOfWeek <= DayOfWeek.Friday)
+                {
+                    if (datetimeEST.TimeOfDay >= new TimeSpan(9, 0, 0) && datetimeEST.TimeOfDay <= new TimeSpan(17, 0, 0) && datetimeEST2.TimeOfDay >= new TimeSpan(9, 0, 0) && datetimeEST2.TimeOfDay <= new TimeSpan(17, 0, 0))
+                    {
+
+                        Connection.AppointmentAdd(CustomerIDCB1.Text, UserIDCB1.Text, nameTB1.Text, dateTimePicker1.Value, dateTimePicker2.Value);
+                        Connection.AppointmentDatabase(dataXX);
+                        this.Close();
+                        Customer.Instance.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Time has to be between 9AM and 5PM EST");
+                    }
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Time has to be between 9AM and 5PM EST");
+                }
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("The start time has to be before the end time");
+            }
+
+
+
+
         }
+
+        
     }
 }
--- Software 2 Rykeem/ModifyAppointment.cs
+++ Software 2 Rykeem/ModifyAppointment.cs
@@ -21,7 +21,8 @@ namespace Software_2_Rykeem
             dateTimePicker2.Format = DateTimePickerFormat.Custom;
             dateTimePicker1.CustomFormat = "yyyy-MM-dd hh:mm:ss tt";
             dateTimePicker2.CustomFormat = "yyyy-MM-dd hh:mm:ss tt";
-            
+           
+
             Connection.UserIDComboBox(UserIDCB1);
             Connection.CustomerIDComboBox(CustomerIDCB1);
            
@@ -38,12 +39,50 @@ namespace Software_2_Rykeem
 
         private void SaveB1_Click(object sender, EventArgs e)
         {
-            Connection.ModifyAppointment(AppointmentIDTB.Text, CustomerIDCB1.Text, UserIDCB1.Text, nameTB1.Text, dateTimePicker1.Value, dateTimePicker2.Value);
-            Connection.AppointmentDatabase(dataX);
-            this.Close();
-            Customer.Instance.Show();
-            dateTimePicker1.MinDate = DateTime.Today;
-            dateTimePicker2.MinDate = DateTime.Today;
+            
+
+            DateTime dateTime = dateTimePicker1.Value;
+
+
+            TimeZoneInfo est = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime datetimeEST = TimeZoneInfo.ConvertTime(dateTime, TimeZoneInfo.Local, est);
+
+
+            DateTime dateTime2 = dateTimePicker2.Value;
+
+
+            TimeZoneInfo est2 = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime datetimeEST2 = TimeZoneInfo.ConvertTime(dateTime2, TimeZoneInfo.Local, est2);
+
+            if (dateTime < dateTime2)
+            {
+                if (datetimeEST.DayOfWeek >= DayOfWeek.Monday && datetimeEST.DayOfWeek <= DayOfWeek.Friday && datetimeEST2.DayOfWeek >= DayOfWeek.Monday && datetimeEST2.DayOfWeek <= DayOfWeek.Friday)
+                {
+                    if (datetimeEST.TimeOfDay >= new TimeSpan(9, 0, 0) && datetimeEST.TimeOfDay <= new TimeSpan(17, 0, 0) && datetimeEST2.TimeOfDay >= new TimeSpan(9, 0, 0) && datetimeEST2.TimeOfDay <= new TimeSpan(17, 0, 0))
+                    {
+                        Connection.ModifyAppointment(AppointmentIDTB.Text, CustomerIDCB1.Text, UserIDCB1.Text, nameTB1.Text, dateTimePicker1.Value, dateTimePicker2.Value);
+                        Connection.AppointmentDatabase(dataX);
+                        this.Close();
+                        Customer.Instance.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Time has to be between 9AM and 5PM EST");
+                    }
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Time has to be between 9AM and 5PM EST");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("The start time has to be before the end time");
+            }
+
         }
 
         private void CancelB1_Click(object sender, EventArgs e)
